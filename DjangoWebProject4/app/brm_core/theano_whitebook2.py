279cf38a1aa878214fbf7ea51d56f3f666f6bbf9
zeroprg <zeroprg@Aspire>
2018-03-31 22:13:24 -0600

    Completed abstract evaluation logic for generic boolean functions ( contains >,<,=) for symbolic calc.Tested

---
+++ DjangoWebProject4/app/brm_core/theano_whitebook2.py
@@ -0,0 +1,93 @@
+from theano import tensor as T
+import theano, time, numpy
+
+
+
+
+class RuleEvaluator(object):
+    """A RuleEvalutor evaluate symbolic rule . RuleEvaluator have the
+    following properties:
+
+    Attributes:
+        python_code_rule: A string of the Python code representing formula.
+        operand: A simple operand '<', '>'. '-'.
+        args: list of symbolic variables (string), init. inside by Theano library
+        
+    """
+
+    def __init__(self, python_code_rule, operand, args,rows,cols):
+        """Return a Customer object whose name is *name* and starting
+        balance is *balance*."""
+        self.rule = python_code_rule
+        
+       # just case 
+        constant = args[len(args)-1] # constant is last arg
+        self.operands = {'<' : 'T.gt(self.param,'+ constant +')',
+                         '>' : 'T.lt(self.param,'+ constant +')',
+                         '=' : 'T.eq(self.param,'+ constant +')'
+                        }
+        self.operand = self.operands[operand]
+        self.args = args
+        self.args_str = (','.join(args))
+        #print(self.args_str)
+        for p in self.args:
+            globals()[p] = T.dmatrix(p)
+        #evaluate list of parameters    
+        self._list = eval('[' + self.args_str + ']')
+        #print(a)
+        
+        self.param = T.dmatrix('param')
+        self.ones = numpy.ones((rows,cols))
+        self.zeros = numpy.zeros((rows,cols))
+        self.f_switch = theano.function(self._list, self.__rule(self._list),
+                            mode=theano.Mode(linker='vm'))
+
+        
+ 
+        
+        
+            
+    """This very generic rule evaluated during construction class
+    ."""
+    def __rule(self,p1,*therest):
+        print(self.rule)
+        self.param = eval(self.rule)    
+        self.param = T.switch(eval(self.operand), self.zeros, self.ones)
+        return self.param
+    
+        
+        
+
+    def evaluate(self,p1,*therest):
+        """Return the function which available to apply arguments and run
+        ."""
+        ret = self.f_switch(p1,*therest) 
+        return ret
+
+
+
+
+# Tests
+rows,cols = 2,2
+
+m1 = numpy.random.randint(5,size=(rows,cols))
+m2 = numpy.random.randint(5,size=(rows,cols))
+m3 = numpy.random.randint(5,size=(rows,cols))
+m4 = numpy.random.randint(5,size=(rows,cols))
+m5 = numpy.random.randint(5,size=(rows,cols))
+
+
+re = RuleEvaluator('a-b','>',['a','b'],rows,cols)
+
+
+c =  re.evaluate(m3,m5)
+
+print ('a: ' ) 
+print (m3) 
+print ('b: ')
+print (m5)
+print( 'a-b>0: ')
+print (c)
+ 
+
+
