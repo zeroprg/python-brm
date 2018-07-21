775abe663a27c14c832b05cce8b58cb0b930e3ea
Vitaly Arkhipov <zeroprg@yahoo.com>
2018-07-21 17:16:57 +0000

    Debug mode off

--- main/app/brm_core/httpserver.py
+++ main/app/brm_core/httpserver.py
@@ -184,5 +184,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if (__name__ == '__main__'):
-    app.run(debug=True) 
+    app.run(host='0.0.0.0',threaded=True,port=8080) 
 
